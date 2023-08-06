db18e7f41c35dc9a24d6f6dd8a9d7f1e7260a462
PavelZolin <goldzolingold@gmail.com>
2023-08-06 22:25:12 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://398bf00b-4083-4ad5-9dc0-6d13d59255b6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
